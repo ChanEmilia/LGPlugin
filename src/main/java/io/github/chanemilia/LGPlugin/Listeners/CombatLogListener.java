142b4ea59ff63a2180ee8f3b1f23ccbbe32d9052
Emilia <ecsr08@outlook.com>
2026-02-01 13:19:20 +0000

    Disable elytra in combat Part 2

--- src/main/java/io/github/chanemilia/LGPlugin/Listeners/CombatLogListener.java
+++ src/main/java/io/github/chanemilia/LGPlugin/Listeners/CombatLogListener.java
@@ -5,6 +5,7 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
