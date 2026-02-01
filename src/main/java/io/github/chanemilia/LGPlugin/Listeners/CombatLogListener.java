777cfdc0896ccd532308490d0afa17459fadaf9f
Emilia <ecsr08@outlook.com>
2026-02-01 13:11:30 +0000

    Disable elytra in combat

--- src/main/java/io/github/chanemilia/LGPlugin/Listeners/CombatLogListener.java
+++ src/main/java/io/github/chanemilia/LGPlugin/Listeners/CombatLogListener.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityToggleGlideEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerKickEvent;
